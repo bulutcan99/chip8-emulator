f51b72cd9732f51a315355f4166506ab1d031a28
bulutcan99 <gocerbulutcan@gmail.com>
2024-10-07 16:37:07 +0300

    stack push pop

--- crates/libs/chip8_core/src/memory.rs
+++ crates/libs/chip8_core/src/memory.rs
@@ -10,14 +10,17 @@ const NUM_REGS: usize = 16;
 // Screen refresh rate (frames per second)
 const REFRESH_RATE: usize = 60;
 
+// Chip-8 standard that the beginning of all Chip-8 programs will be loaded in starting at RAM address 0x200.
+const START_ADDR: u16 = 0x200;
+
 pub struct Memory {
 	// Will address from 0x000 (0) to 0x1ff (511)
 	// ROM data will get after first 512 byte
 	// 4096 bytes total
-	ram: [u8; RAM_SIZE],
+	pub ram: [u8; RAM_SIZE],
 
 	// The stack allows for up to *STACK_SIZE* nested subroutines.
-	stack: [u16; STACK_SIZE],
+	pub(crate) stack: [u16; STACK_SIZE],
 
 	// Variable register; General purpose registers.
 	v_reg: [u8; NUM_REGS],
@@ -26,10 +29,10 @@ pub struct Memory {
 	i_reg: u16,
 
 	// Stack pointer; used to point to the top level of the stack.
-	sp: u8,
+	pub(crate) sp: u8,
 
 	// Program counter; stores the memory of the current executed instruction.
-	pc: u16,
+	pub(crate) pc: u16,
 
 	// Delay timer; if non-zero (activated), will decrease by 1 at *REFRESH_RATE*
 	// until reaches zero (deactivated).
@@ -47,12 +50,13 @@ impl Memory {
 			v_reg: [0; NUM_REGS],
 			i_reg: 0,
 			sp: 0,
-			pc: 0x200, // Default pc address for CHIP8
+			pc: START_ADDR, // Default pc address for CHIP8
 			dt: 0,
 			st: 0,
 		}
 	}
-	pub fn get_reserved_memory(&self) -> &[u8] {
+
+	fn get_reserved_memory(&self) -> &[u8] {
 		&self.ram[0..512]
 	}
 }
