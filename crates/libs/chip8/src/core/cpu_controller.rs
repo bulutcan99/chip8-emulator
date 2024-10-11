13620382ee13a1ad0a1ad2797f309b6d6dfb374b
bulutcan99 <gocerbulutcan@gmail.com>
2024-10-11 15:12:22 +0300

    refactor

--- crates/libs/chip8/src/core/cpu_controller.rs
+++ crates/libs/chip8/src/core/cpu_controller.rs
@@ -102,8 +102,8 @@ impl CpuController {
 		BitManipulation::combine_nibbles_to_16bit_address(x, y, fourth)
 	}
 
-	fn skip_next_instruction(&self, mem_ctr: &mut Emulator) {
-		mem_ctr.inc_pc_by(2)
+	fn skip_next_instruction(&self, emu: &mut Emulator) {
+		emu.inc_pc_by(2)
 	}
 
 	// **INSTRUCTIONS**
@@ -116,27 +116,27 @@ impl CpuController {
 	// 00E0 - CLS -> will implement after sdl
 
 	// 00EE - RET (Return from Subroutine)
-	fn return_from_subroutine(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn return_from_subroutine(&self, emu: &mut Emulator) -> Result<(), Error> {
 		// Return to the previous function by popping the stack.
-		mem_ctr.stack_pop().map_err(|err| {
+		emu.stack_pop().map_err(|err| {
 			error!("Failed to return from subroutine: {:?}", err);
 			err
 		})
 	}
 
 	// 1NNN - JMP NNN (Jump to Address NNN)
-	fn jump_to_address(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn jump_to_address(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let address = self.extract_12bit_address();
-		mem_ctr.set_pc(address);
+		emu.set_pc(address);
 		Ok(())
 	}
 
 	// 2NNN - CALL NNN (Call Address NNN)
-	fn call_address(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn call_address(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let address = self.extract_12bit_address();
-		match mem_ctr.stack_push(mem_ctr.get_pc()) {
+		match emu.stack_push(emu.get_pc()) {
 			Ok(_) => {
-				mem_ctr.set_pc(address);
+				emu.set_pc(address);
 				Ok(())
 			}
 			Err(err) => {
@@ -150,163 +150,171 @@ impl CpuController {
 	}
 
 	// 3XNN - SE VX
-	fn skip_equal_vx_byte(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn skip_equal_vx_byte(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
-		let v = mem_ctr.get_v(x)?;
+		let v = emu.get_v(x)?;
 		let second_byte = self.second_byte();
 		if v == second_byte {
-			self.skip_next_instruction(mem_ctr)
+			self.skip_next_instruction(emu)
 		}
 		Ok(())
 	}
 
 	// 4XNN - SNE VX
-	fn skip_not_equal_vx_byte(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn skip_not_equal_vx_byte(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
-		let v = mem_ctr.get_v(x)?;
+		let v = emu.get_v(x)?;
 		let second_byte = self.second_byte();
 		if v != second_byte {
-			self.skip_next_instruction(mem_ctr)
+			self.skip_next_instruction(emu)
 		}
 		Ok(())
 	}
 
 	// 5XY0 - SE VX == VY
-	fn skip_equal_vx_vy(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn skip_equal_vx_vy(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
-		let vx = mem_ctr.get_v(x)?;
+		let vx = emu.get_v(x)?;
 		let y = self.x();
-		let vy = mem_ctr.get_v(y)?;
+		let vy = emu.get_v(y)?;
 		if vx == vy {
-			self.skip_next_instruction(mem_ctr)
+			self.skip_next_instruction(emu)
 		}
 		Ok(())
 	}
 
 	// 6XNN - LD VX with byte
-	fn load_vx_with_byte(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn load_vx_with_byte(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let second_byte = self.second_byte();
-		mem_ctr.set_v(x, second_byte)
+		emu.set_v(x, second_byte)
 	}
 
 	// 7XNN - ADD VX, NN
-	fn add_vx_with_byte(&mut self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn add_vx_with_byte(&mut self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let nn = self.second_byte();
 
-		let vx = mem_ctr.get_v(x)?;
+		let vx = emu.get_v(x)?;
 		let result = vx.wrapping_add(nn);
 
-		mem_ctr.set_v(x, result)?;
+		emu.set_v(x, result)?;
 
 		Ok(())
 	}
 
 	// 8XY0 - LD VX with VY
-	fn load_vx_with_vy(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn load_vx_with_vy(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let y = self.x();
-		let vy = mem_ctr.get_v(y)?;
-		mem_ctr.set_v(x, vy)?;
+		let vy = emu.get_v(y)?;
+		emu.set_v(x, vy)?;
 		Ok(())
 	}
 
 	// 8XY1 - LD VX with (VX or VY)
-	fn load_vx_with_vx_or_vy(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn load_vx_with_vx_or_vy(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let y = self.y();
-		let vx = mem_ctr.get_v(x)?;
-		let vy = mem_ctr.get_v(y)?;
-		mem_ctr.set_v(x, vx | vy)?;
+		let vx = emu.get_v(x)?;
+		let vy = emu.get_v(y)?;
+		emu.set_v(x, vx | vy)?;
 		Ok(())
 	}
 
 	// 8XY2 - LD VX with (VX and VY)
-	fn load_vx_with_vx_and_vy(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn load_vx_with_vx_and_vy(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let y = self.y();
-		let vx = mem_ctr.get_v(x)?;
-		let vy = mem_ctr.get_v(y)?;
-		mem_ctr.set_v(x, vx & vy)?;
+		let vx = emu.get_v(x)?;
+		let vy = emu.get_v(y)?;
+		emu.set_v(x, vx & vy)?;
 		Ok(())
 	}
 
 	// 8XY3 - LD VX with (VX xor VY)
-	fn load_vx_with_vx_xor_vy(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn load_vx_with_vx_xor_vy(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let y = self.y();
-		let vx = mem_ctr.get_v(x)?;
-		let vy = mem_ctr.get_v(y)?;
-		mem_ctr.set_v(x, vx ^ vy)?;
+		let vx = emu.get_v(x)?;
+		let vy = emu.get_v(y)?;
+		emu.set_v(x, vx ^ vy)?;
 		Ok(())
 	}
 
 	// 8XY4 - ADD VX, VY
-	fn add_vx_with_vy(&mut self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn add_vx_with_vy(&mut self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let y = self.y();
 
-		let vx = mem_ctr.get_v(x)?;
-		let vy = mem_ctr.get_v(y)?;
+		let vx = emu.get_v(x)?;
+		let vy = emu.get_v(y)?;
 
 		let (result, overflow) = vx.overflowing_add(vy);
 
 		if overflow {
-			mem_ctr.set_v(0xF, 1)?
+			emu.set_v(0xF, 1)?
 		} else {
-			mem_ctr.set_v(0xF, 0)?;
+			emu.set_v(0xF, 0)?;
 		}
 
-		mem_ctr.set_v(x, result)?;
+		emu.set_v(x, result)?;
 
 		Ok(())
 	}
 
 	// 8XY5 - SUB VX, VY
-	fn sub_vx_to_vy(&mut self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn sub_vx_to_vy(&mut self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
 		let y = self.y();
 
-		let vx = mem_ctr.get_v(x)?;
-		let vy = mem_ctr.get_v(y)?;
+		let vx = emu.get_v(x)?;
+		let vy = emu.get_v(y)?;
 
 		let (result, overflow) = vx.overflowing_sub(vy);
 
 		if overflow {
-			mem_ctr.set_v(0xF, 0)?;
+			emu.set_v(0xF, 0)?;
 		} else {
-			mem_ctr.set_v(0xF, 1)?;
+			emu.set_v(0xF, 1)?;
 		}
 
-		mem_ctr.set_v(x, result)?;
+		emu.set_v(x, result)?;
 
 		Ok(())
 	}
 
 	// 8XY6 - SHIFT VX (to the right) by 1
-	fn shift_vx_to_right(&mut self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	fn shift_vx_to_right(&mut self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
-		let vx = mem_ctr.get_v(x)?;
+		let vx = emu.get_v(x)?;
 		let lsb = vx & 0b0000_0001;
 
-		mem_ctr.set_v(0xF, lsb)?;
+		emu.set_v(0xF, lsb)?;
 		let result = vx >> 1;
-		mem_ctr.set_v(x, result)?;
+		emu.set_v(x, result)?;
 
 		Ok(())
 	}
 
-	// 8XY7 - MINUS VX = VY-VX
-	fn minus_vx_from_vy(&self, mem_ctr: &mut Emulator) -> Result<(), Error> {
+	// 8XY7 - MINUS VX = VY - VX
+	fn minus_vx_from_vy(&self, emu: &mut Emulator) -> Result<(), Error> {
 		let x = self.x();
-		let vx = mem_ctr.get_v(x)?;
+		let vx = emu.get_v(x)?;
 
 		let y = self.y();
-		let vy = mem_ctr.get_v(y)?;
+		let vy = emu.get_v(y)?;
 
 		let (result, overflow) = vy.overflowing_sub(vx);
 
-		if overflow {}
+		// Set carry flag (VF) based on overflow
+		if overflow {
+			emu.set_v(0xF, 0)?;
+		} else {
+			emu.set_v(0xF, 1)?;
+		}
+
+		emu.set_v(x, result)?;
+		Ok(())
 	}
 }
