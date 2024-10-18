a401ca19856e8827d1a449a86ce8437309324a1e
bulutcan99 <gocerbulutcan@gmail.com>
2024-10-18 14:30:34 +0300

    refactor

--- crates/libs/chip8/src/core/instruction.rs
+++ crates/libs/chip8/src/core/instruction.rs
@@ -9,167 +9,165 @@ use super::{
 
 //TODO: GET_RAM WILL CHANGE TO SET_RAM (for sets)
 pub enum Instruction {
-    Nop,
-    Cls,
-    Ret,
-    Jmp(u16),
-    Call(u16),
-    SeVx(u8, u8),
-    SneVx(u8, u8),
-    SeVxVy(u8, u8),
-    LdVx(u8, u8),
-    AddVx(u8, u8),
-    LdVxVy(u8, u8),
-    OrVxVy(u8, u8),
-    AndVxVy(u8, u8),
-    XorVxVy(u8, u8),
-    AddVxVy(u8, u8),
-    SubVxVy(u8, u8),
-    ShrVx(u8),
-    SubnVxVy(u8, u8),
-    ShlVx(u8),
-    SneVxVy(u8, u8),
-    LdI(u16),
-    JpV0(u16),
-    Rnd(u8, u8),
-    Drw(u8, u8, u8),
-    SkpVx(u8),
-    SknpVx(u8),
-    LdVxDt(u8),
-    LdVxK(u8),
-    LdDtVx(u8),
-    LdStVx(u8),
-    AddIVx(u8),
-    LdFVx(u8),
-    LdBVx(u8),
-    LdIVx(u8),
-    LdVxI(u8),
+    Op0000,
+    Op00E0,
+    Op00EE,
+    Op1NNN(u16),
+    Op2NNN(u16),
+    Op3XNN(u8, u8),
+    Op4XNN(u8, u8),
+    Op5XY0(u8, u8),
+    Op6XNN(u8, u8),
+    Op7XNN(u8, u8),
+    Op8XY0(u8, u8),
+    Op8XY1(u8, u8),
+    Op8XY2(u8, u8),
+    Op8XY3(u8, u8),
+    Op8XY4(u8, u8),
+    Op8XY5(u8, u8),
+    Op8XY6(u8),
+    Op8XY7(u8, u8),
+    Op8XYE(u8),
+    Op9XY0(u8, u8),
+    OpANNN(u16),
+    OpBNNN(u16),
+    OpCXNN(u8, u8),
+    OpDXYN(u8, u8, u8),
+    OpEX9E(u8),
+    OpEXA1(u8),
+    OpFX07(u8),
+    OpFX0A(u8),
+    OpFX15(u8),
+    OpFX18(u8),
+    OpFX1E(u8),
+    OpFX29(u8),
+    OpFX33(u8),
+    OpFX55(u8),
+    OpFX65(u8),
 }
 
 impl Instruction {
     pub fn execute(&self, emu: &mut Emulator, is_inc_pc: &mut bool) -> Result<(), Error> {
         match self {
-            Instruction::Nop => {}
-            Instruction::Cls => {
+            Instruction::Op0000 => {} // NOP
+            Instruction::Op00E0 => {
                 emu.clear_screen();
             }
-            Instruction::Ret => {
+            Instruction::Op00EE => {
                 emu.stack_pop().map_err(|err| {
                     error!("Failed to return from subroutine: {:?}", err);
                     err
                 })?;
             }
-            Instruction::Jmp(addr) => {
+            Instruction::Op1NNN(addr) => {
                 emu.set_pc(*addr);
-                *is_inc_pc = false; // Set to false
+                *is_inc_pc = false;
             }
-            Instruction::Call(addr) => {
+            Instruction::Op2NNN(addr) => {
                 emu.stack_push(emu.get_pc())?;
                 emu.set_pc(*addr);
-                *is_inc_pc = false; // Set to false
+                *is_inc_pc = false;
             }
-            Instruction::SeVx(x, byte) => {
+            Instruction::Op3XNN(x, byte) => {
                 let v = emu.get_v(*x)?;
-                // Example implementation
                 if v == *byte {
                     emu.inc_pc_by(2);
                 }
             }
-            Instruction::SneVx(x, byte) => {
+            Instruction::Op4XNN(x, byte) => {
                 let v = emu.get_v(*x)?;
-                // Example implementation
                 if v != *byte {
                     emu.inc_pc_by(2);
                 }
             }
-            Instruction::SeVxVy(x, y) => {
+            Instruction::Op5XY0(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 if vx == vy {
                     emu.inc_pc_by(2);
                 }
             }
-            Instruction::LdVx(x, byte) => {
+            Instruction::Op6XNN(x, byte) => {
                 emu.set_v(*x, *byte)?;
             }
-            Instruction::AddVx(x, byte) => {
+            Instruction::Op7XNN(x, byte) => {
                 let vx = emu.get_v(*x)?;
                 let (result, _) = vx.overflowing_add(*byte);
                 emu.set_v(*x, result)?;
             }
-            Instruction::LdVxVy(x, y) => {
+            Instruction::Op8XY0(x, y) => {
                 let vy = emu.get_v(*y)?;
                 emu.set_v(*x, vy)?;
             }
-            Instruction::OrVxVy(x, y) => {
+            Instruction::Op8XY1(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 emu.set_v(*x, vx | vy)?;
             }
-            Instruction::AndVxVy(x, y) => {
+            Instruction::Op8XY2(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 emu.set_v(*x, vx & vy)?;
             }
-            Instruction::XorVxVy(x, y) => {
+            Instruction::Op8XY3(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 emu.set_v(*x, vx ^ vy)?;
             }
-            Instruction::AddVxVy(x, y) => {
+            Instruction::Op8XY4(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 let (result, overflow) = vx.overflowing_add(vy);
                 emu.set_v(0xF, if overflow { 1 } else { 0 })?;
                 emu.set_v(*x, result)?;
             }
-            Instruction::SubVxVy(x, y) => {
+            Instruction::Op8XY5(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 let (result, overflow) = vx.overflowing_sub(vy);
                 emu.set_v(0xF, if overflow { 0 } else { 1 })?;
                 emu.set_v(*x, result)?;
             }
-            Instruction::ShrVx(x) => {
+            Instruction::Op8XY6(x) => {
                 let vx = emu.get_v(*x)?;
                 let lsb = vx & 0b0000_0001;
                 emu.set_v(0xF, lsb)?;
                 let result = vx >> 1;
                 emu.set_v(*x, result)?;
             }
-            Instruction::SubnVxVy(x, y) => {
+            Instruction::Op8XY7(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 let (result, overflow) = vy.overflowing_sub(vx);
                 emu.set_v(0xF, if overflow { 0 } else { 1 })?;
                 emu.set_v(*x, result)?;
             }
-            Instruction::ShlVx(x) => {
+            Instruction::Op8XYE(x) => {
                 let vx = emu.get_v(*x)?;
                 let msb = (vx & 0b10000000) >> 7;
                 emu.set_v(0xF, msb)?;
                 let result = vx << 1;
                 emu.set_v(*x, result)?;
             }
-            Instruction::SneVxVy(x, y) => {
+            Instruction::Op9XY0(x, y) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 if vx != vy {
                     emu.inc_pc_by(2);
                 }
             }
-            Instruction::LdI(addr) => {
+            Instruction::OpANNN(addr) => {
                 emu.set_i(*addr);
             }
-            Instruction::JpV0(addr) => {
+            Instruction::OpBNNN(addr) => {
                 let v0 = emu.get_v(0)?;
                 emu.set_pc((*addr).wrapping_add(v0 as u16));
             }
-            Instruction::Rnd(x, byte) => {
+            Instruction::OpCXNN(x, byte) => {
                 let rnd = rand::thread_rng().gen_range(0..=255);
                 emu.set_v(*x, rnd & *byte)?;
             }
-            Instruction::Drw(x, y, nibble) => {
+            Instruction::OpDXYN(x, y, nibble) => {
                 let vx = emu.get_v(*x)?;
                 let vy = emu.get_v(*y)?;
                 let rows = *nibble;
@@ -194,52 +192,51 @@ impl Instruction {
                     emu.set_v(0xF, 0)?;
                 }
             }
-            Instruction::SkpVx(x) => {
+            Instruction::OpEX9E(x) => {
                 let vx = emu.get_v(*x)?;
-
                 let is_pressed = emu.is_key_pressed(vx);
                 if is_pressed {
                     emu.inc_pc_by(2);
                 }
             }
-            Instruction::SknpVx(x) => {
+            Instruction::OpEXA1(x) => {
                 let vx = emu.get_v(*x)?;
                 let is_pressed = emu.is_key_pressed(vx);
                 if !is_pressed {
                     emu.inc_pc_by(2);
                 }
             }
-            Instruction::LdVxDt(x) => {
+            Instruction::OpFX07(x) => {
                 let dt = emu.get_dt();
                 emu.set_v(*x, dt)?;
             }
-            Instruction::LdVxK(x) => {
+            Instruction::OpFX0A(x) => {
                 if let Some(key) = emu.check_key_press() {
                     emu.set_v(*x, key)?;
                 } else {
                     emu.dec_pc_by(2);
-                    *is_inc_pc = false; // Set to false
+                    *is_inc_pc = false;
                 }
             }
-            Instruction::LdDtVx(x) => {
+            Instruction::OpFX15(x) => {
                 let vx = emu.get_v(*x)?;
                 emu.set_dt(vx);
             }
-            Instruction::LdStVx(x) => {
+            Instruction::OpFX18(x) => {
                 let vx = emu.get_v(*x)?;
                 emu.set_st(vx);
             }
-            Instruction::AddIVx(x) => {
+            Instruction::OpFX1E(x) => {
                 let vx = emu.get_v(*x)?;
                 let i = emu.get_i();
                 emu.set_i(i.wrapping_add(vx as u16));
             }
-            Instruction::LdFVx(x) => {
+            Instruction::OpFX29(x) => {
                 let vx = emu.get_v(*x)?;
                 let f = 5 * vx as u16;
                 emu.set_i(f);
             }
-            Instruction::LdBVx(x) => {
+            Instruction::OpFX33(x) => {
                 let vx = emu.get_v(*x)?;
                 let hundreds = (vx / 100) as u8;
                 let tens = (vx / 10) % 10 as u8;
@@ -249,14 +246,14 @@ impl Instruction {
                 emu.set_to_ram(emu.get_i() as usize + 1, tens)?;
                 emu.set_to_ram(emu.get_i() as usize + 2, ones)?;
             }
-            Instruction::LdIVx(x) => {
+            Instruction::OpFX55(x) => {
                 let i = emu.get_i();
                 for index in 0..=*x {
                     let vx = emu.get_v(index)?;
                     emu.set_to_ram(i as usize + index as usize, vx)?;
                 }
             }
-            Instruction::LdVxI(x) => {
+            Instruction::OpFX65(x) => {
                 let i = emu.get_i();
                 for idx in 0..=*x {
                     let value = emu.get_ram()[i as usize + idx as usize];
