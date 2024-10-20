d23ee55a05bfd4252159adbaa3f0811f0df41458
bulutcan99 <gocerbulutcan@gmail.com>
2024-10-20 16:01:10 +0300

    refactor

--- crates/libs/chip8/src/core/chip8.rs
+++ crates/libs/chip8/src/core/chip8.rs
@@ -1,60 +1,28 @@
-// Total size of the Chip-8 system's memory (in bytes)
+use std::default::Default;
+
 const RAM_SIZE: usize = 4096;
-
-// Size of the CPU's call stack (for storing return addresses)
 const STACK_SIZE: usize = 16;
-
-// Number of general-purpose registers available in the Chip-8 CPU
 const NUM_REGS: usize = 16;
-
-// Screen refresh rate (frames per second)
-const REFRESH_RATE: usize = 60;
-
-// Chip-8 standard that the beginning of all Chip-8 programs will be loaded in starting at RAM address 0x200.
 const START_ADDR: u16 = 0x200;
-
-// Default display resolution.
 pub const SCREEN_WIDTH: usize = 64;
 pub const SCREEN_HEIGHT: usize = 32;
 
 pub struct CHIP8 {
-    // Will address from 0x000 (0) to 0x1ff (511)
-    // ROM data will get after first 512 byte
-    // 4096 bytes total
     pub ram: [u8; RAM_SIZE],
-
-    // The stack allows for up to *STACK_SIZE* nested subroutines.
     pub stack: [u16; STACK_SIZE],
-
-    // Variable register; General purpose registers.
     pub v_reg: [u8; NUM_REGS],
-
-    // Index register; acts as a pointer for accessing and manipulating memory
     pub i_reg: u16,
-
-    // Stack pointer; used to point to the top level of the stack.
     pub sp: u8,
-
-    // Program counter; stores the memory of the current executed instruction.
     pub pc: u16,
-
-    // Delay timer; if non-zero (activated), will decrease by 1 at *REFRESH_RATE*
-    // until reaches zero (deactivated).
     pub dt: u8,
-
-    // Sound timer; if non-zero (activated) sounds the buzzer sound.
     pub st: u8,
-
-    // Keypad (16 keys, 0x0-0xF)
     pub keys: [bool; 16],
-
-    // Display (64x32 pixels, true = pixel on, false = pixel off)
     pub display: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
 }
 
-impl CHIP8 {
-    pub fn new() -> Self {
-        let new_chip8 = Self {
+impl Default for CHIP8 {
+    fn default() -> Self {
+        Self {
             ram: [0; RAM_SIZE],
             stack: [0; STACK_SIZE],
             v_reg: [0; NUM_REGS],
@@ -65,30 +33,12 @@ impl CHIP8 {
             st: 0,
             keys: [false; 16],
             display: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
-        };
-
-        new_chip8
-    }
-
-    pub fn reset(&mut self) {
-        self.ram = [0; RAM_SIZE];
-        self.stack = [0; STACK_SIZE];
-        self.v_reg = [0; NUM_REGS];
-        self.i_reg = 0;
-        self.sp = 0;
-        self.pc = START_ADDR;
-        self.dt = 0;
-        self.st = 0;
-        self.keys = [false; 16];
-        self.display = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
-    }
-
-    fn get_reserved_memory(&self) -> &[u8] {
-        &self.ram[0..512]
-    }
-
-    fn keypress(&mut self, idx: u8, is_pressed: bool) {
-        self.keys[idx as usize] = is_pressed;
+        }
     }
 }
 
+impl CHIP8 {
+    pub fn reset(&mut self) {
+        *self = Self::default();
+    }
+}
