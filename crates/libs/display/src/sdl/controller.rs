825ea1bf2b2630dbecdc82419c5eb453666f550a
bulutcan99 <gocerbulutcan@gmail.com>
2024-10-21 16:35:42 +0300

    feat

--- crates/libs/display/src/sdl/controller.rs
+++ crates/libs/display/src/sdl/controller.rs
@@ -1,1 +1,11 @@
-pub struct Controller {}
+use super::window::CustomWindow;
+
+pub struct Controller<'a> {
+    window: &'a CustomWindow<'a>,
+}
+
+impl<'a> Controller<'a> {
+    pub fn new(window: &'a CustomWindow<'a>) -> Self {
+        Self { window }
+    }
+}
