64cde0e2012a7f19b9aa8db5ce9374397afaeaa8
bulutcan99 <gocerbulutcan@gmail.com>
2024-10-22 19:05:36 +0300

    refactor

--- crates/libs/chip8/src/core/display.rs
+++ crates/libs/chip8/src/core/display.rs
@@ -14,41 +14,6 @@ impl<'a> DisplayController<'a> {
         Self { window }
     }
 
-    pub fn pixel_at(&mut self, x: u8, y: u8, emu: &mut Emulator) -> Result<(), Error> {
-        // Wrap the coordinates to fit within the window dimensions.
-        let x = Math2d::wrap_coord(x, self.window.win_w);
-        let y = Math2d::wrap_coord(y, self.window.win_h);
-        let pixel_index = (y as u32 * self.window.win_w) + x as u32;
-
-        // Determine if the pixel is OFF (0) or ON (1) and choose the color accordingly.
-        let pixel_is_off = self.window.pixel_vec[pixel_index as usize] == 0;
-        let draw_color = if pixel_is_off {
-            self.window.pixel_color
-        } else {
-            // If pixel is ON, check and set the collision flag (VF).
-            if emu.get_v(0xF)? == 0 {
-                emu.set_v(0xF, 1)?;
-            }
-            self.window.bg_color
-        };
-
-        // Set the draw color and draw the point.
-        self.window.canvas.set_draw_color(draw_color);
-        self.window
-            .canvas
-            .draw_point(Point::new(x as i32, y as i32))
-            .map_err(|e| anyhow!("Error drawing point: {:?}", e))?;
-        // Toggle the pixel state (ON/OFF).
-        self.window.pixel_vec[pixel_index as usize] ^= 1;
-
-        Ok(())
-    }
-    pub fn clear_screen(&mut self) {
-        self.window.pixel_vec.fill(0);
-        self.window.canvas.set_draw_color(self.window.bg_color);
-        self.window.canvas.clear();
-    }
-
     pub fn display_canvas(&mut self) {
         self.window.canvas.present();
     }
